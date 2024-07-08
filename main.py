637a3ef1f129be31d869e26c5cb001183de8b3f5
Morineyor <shashkoff.danilka@gmail.com>
2024-07-08 22:09:11 +0300

    dark mode

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 from nicegui import ui
 
+dark = ui.dark_mode()
+ui.label('Switch mode: ')
+ui.button('Dark', on_click=dark.enable)
+ui.button('Light', on_click=dark.disable)
+
 table = ui.aggrid({
     'defaultColDef': {'flex': 1},
     'columnDefs': [
