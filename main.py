9e2ac283faf008fcda0b337bc600482d8e9a1899
Morineyor <shashkoff.danilka@gmail.com>
2024-07-08 19:32:20 +0300

    Upd

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import time
 from nicegui import ui
 
+table = ui.aggrid({
+    'defaultColDef': {'flex': 1},
+    'columnDefs': [
+        {'headerName': 'Name', 'field': 'name'},
+        {'headerName': 'Price', 'field': 'price'},
+        {'headerName': 'Quantity', 'field': 'quantity'},
+    ],
+    'rowData': [
+        {'name': 'Тактический запас', 'price': 29000, 'quantity': 15},
+    ],
+    'rowSelection': 'multiple',
+}).classes('max-h-40')
+
+def update():
+    table.options['rowData'][0]['price'] += 1
+    table.update()
+
+
+ui.button(text='Редактировать', on_click=update)
+
+ui.run()
